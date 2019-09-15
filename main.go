c82e6113687328b8b0bc1cc1e41e9f77ce370299
zhiwen <zhiwen@kooboo.cn>
2019-09-15 21:48:02 +0800

    router

--- main.go
+++ main.go
@@ -7,16 +7,21 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/", handleHome)
-	r.GET("/json", handleJson)
+	r.GET("/json", json)
+	r.GET("/asciiJSON", asciiJSON)
+	r.GET("/pureJson", pureJson)
 
-	_ = r.Run(":8081")
+	_ = r.Run(":8080")
 }
 
-func handleJson(c *gin.Context) {
-	c.AsciiJSON(200, gin.H{"123": "测试"})
+func pureJson(c *gin.Context) {
+	c.PureJSON(200, gin.H{"123": "<script>666测试</script>"})
 }
 
-func handleHome(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "666"})
+func asciiJSON(c *gin.Context) {
+	c.AsciiJSON(200, gin.H{"123": "<script>666测试</script>"})
+}
+
+func json(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "<script>666测试</script>"})
 }
